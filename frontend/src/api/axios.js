3d75a2c4e862610f05c4d4b4e569ad4b14041f55
Chitra C <chitrac20@gmail.com>
2025-05-05 11:54:22 +0100

    Added axios api setup file

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4000/api/beans',
+  headers: { 'Content-Type': 'application/json' },
+});
+
+instance.interceptors.response.use(
+  res => res,
+  error => {
+    console.error('API error:', error?.response?.data || error.message);
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
